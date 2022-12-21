be92bf0072d6bfbc67333e4cd26061ed53bfdfc0
Rahul <41139835+rahulrs21@users.noreply.github.com>
2022-12-21 14:46:01 +0400

    updated Banner 2
    Updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -75,7 +75,7 @@ function Banner() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
+          <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:my-14 2xl:my-32">
             <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl">
               Your Premium <br /> Sound, Unplugged!
             </h1>
